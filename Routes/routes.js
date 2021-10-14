23b725b9568a6baaf4257212d316ebf32397b127
agusslam <agus.slamethermawan@gmail.con>
2021-10-14 20:09:58 +0700

    sukses dengan metode JSON parser

--- Routes/routes.js
+++ Routes/routes.js
@@ -7,13 +7,24 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //test POST data
 routes.get('/', (req, res) => {
-    res.render('view', { qs: req.query })
+    res.render('view')
 })
-
 routes.post('/post', urlencodedParser, function (req, res) {
     console.log(req.body);
     res.render('post', { data: req.body });
     // console.log(req.body);
 });
+
+
+//test POST data with JSON
+routes.get('/json', (req, res) => {
+    res.render('viewjson')
+})
+routes.post('/post-json', urlencodedParser, function (req, res) {
+    console.log(req.body);
+    res.render('postjson', { data: req.body });
+    // console.log(req.body);
+});
+
 
 module.exports = routes
